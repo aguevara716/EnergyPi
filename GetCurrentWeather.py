ea350062ee47dd5941d20655a508749710779ff9
Angel Guevara, Jr <aguevara.716@gmail.com>
2020-01-26 01:00:19 -0500

    Initial code import

---
+++ GetCurrentWeather.py
@@ -0,0 +1,51 @@
+import urllib.request
+import urllib.parse
+import json
+import mysql.connector as mariadb
+from datetime import datetime
+from configparser import ConfigParser
+
+def get_current_conditions():
+    config = ConfigParser()
+    config.read("weather-settings.ini")
+    latitude = config.getfloat("main", "latitude")
+    longitude = config.getfloat("main", "longitude")
+    api_key = config.get("main", "darksky_api_key")
+
+    url = f"https://api.darksky.net/forecast/{api_key}/{latitude},{longitude}"
+    response = urllib.request.urlopen(url)
+    json_response = response.read().decode("utf-8")
+    json_obj = json.loads(json_response)
+    current_conditions = json_obj["currently"]
+    return current_conditions
+
+
+def get_timestamp_as_string():
+    now = datetime.now()
+    current_time = now.strftime("%Y-%m-%d %H:%M:%S")
+    return current_time
+
+
+def write_to_database(timestamp, temperature_f, humidity):
+    config = ConfigParser()
+    config.read("database-settings.ini")
+    username = config.get("main", "user")
+    password = config.get("main", "password")
+    database = config.get("main", "database")
+    host = config.get("main", "host")
+
+    mariadb_connection = mariadb.connect(user=username, password=password, database=database, host=host)
+    sqlCommand = f"INSERT INTO WeatherLogs (Timestamp, TemperatureFahrenheit, Humidity) VALUES ('{timestamp}', {temperature_f}, {humidity})"
+    print(sqlCommand)
+    cursor = mariadb_connection.cursor()
+    cursor.execute(sqlCommand)
+    mariadb_connection.commit()
+    mariadb_connection.close()
+
+
+current_conditions = get_current_conditions()
+timestamp = get_timestamp_as_string()
+temperature_f = current_conditions["temperature"]
+humidity = current_conditions["humidity"]
+
+write_to_database(timestamp, temperature_f, humidity)
---
+++ GetEnergyMeterReading.py
@@ -0,0 +1,77 @@
+import subprocess
+import shlex
+import json
+import mysql.connector as mariadb
+from datetime import datetime
+from dateutil import parser
+from configparser import ConfigParser
+
+def run_command(command):
+    print("Launching rtlamr")
+    process = subprocess.Popen(shlex.split(command), stdout=subprocess.PIPE)
+    while True:
+        output = process.stdout.readline()
+        if output == '' and process.poll() is not None:
+            break
+        if output:
+            parse_output(output.strip())
+    rc = process.poll()
+    return rc
+
+
+def parse_output(line):
+    print("Parsing output")
+    json_obj = json.loads(line)
+    timestampString = json_obj["Time"]
+    timestamp = parser.parse(timestampString).strftime("%Y-%m-%d %H:%M:%S")
+    consumption = json_obj["Message"]["Consumption"]/100
+    previous_consumption = get_previous_consumption()
+    write_to_database(timestamp, consumption, previous_consumption)
+
+
+def get_database_connection():
+    config = ConfigParser()
+    config.read("database-settings.ini")
+    user = config.get("main", "user")
+    password = config.get("main", "password")
+    database = config.get("main", "database")
+    host = config.get("main", "host")
+    mariadb_connection = mariadb.connect(user=user, password=password, database=database, host=host)
+    return mariadb_connection
+
+
+def get_previous_consumption():
+    mariadb_connection = get_database_connection()
+    cursor = mariadb_connection.cursor(buffered=True)
+    get_prev_consumption_command = "SELECT TotalConsumption FROM EnergyLogs ORDER BY Timestamp DESC LIMIT 1"
+    print(get_prev_consumption_command)
+    cursor.execute(get_prev_consumption_command)
+    prev_consumption_response = cursor.fetchone()
+    if prev_consumption_response is None:
+        mariadb_connection.close()
+        return 0.0
+    else:
+        prev_consumption = float(prev_consumption_response[0])
+        print(f"previous consumption: {prev_consumption}")
+        mariadb_connection.close()
+        return prev_consumption
+
+
+def write_to_database(timestamp, consumption_kwh, prev_consumption):
+    mariadb_connection = get_database_connection()
+    cursor = mariadb_connection.cursor(buffered=True)
+    delta = 0.0
+    if prev_consumption is not None:
+        delta = consumption_kwh - prev_consumption
+    insert_command = f"INSERT INTO EnergyLogs (Timestamp, TotalConsumption, Delta) VALUES ('{timestamp}', {consumption_kwh}, {delta})"
+    print(insert_command)
+    cursor.execute(insert_command)
+    mariadb_connection.commit()
+    mariadb_connection.close()
+
+
+config = ConfigParser()
+config.read("meter-settings.ini")
+meter_id = config.get("main", "meter_id")
+command = f"rtlamr -filterid={meter_id} -msgtype=scm -format=json -unique"
+run_command(command)
