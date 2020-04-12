5627baae0f510ddb1f6ed70056e13ecd2dc2ee25
Angel Guevara, Jr <aguevara.716@gmail.com>
2020-04-12 13:25:00 -0400

    Updated HourlyLogs to return timestamp as a DateTime instead of a string

--- database/Views/VW_EnergyLogs_HourlyLogs.sql
+++ database/Views/VW_EnergyLogs_HourlyLogs.sql
@@ -3,7 +3,7 @@ DROP VIEW IF EXISTS VW_EnergyLogs_HourlyLogs;
 CREATE VIEW IF NOT EXISTS VW_EnergyLogs_HourlyLogs
 AS
 SELECT
-	DATE_FORMAT(`Timestamp`, '%Y-%m-%d %H:00') AS `Timestamp`,
+	STR_TO_DATE(DATE_FORMAT(`Timestamp`, '%Y-%m-%d %H:00'), '%Y-%m-%d %H:00') AS `Timestamp`,
 	SUM(Delta) AS kWh,
 	COUNT(*) AS DataPoints
 FROM
