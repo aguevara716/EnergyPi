ce8dcc57db2eda0ebf37a4f6839b086c17100682
Angel Guevara, Jr <aguevara.716@gmail.com>
2020-11-15 00:46:02 -0500

    Log Meter Broadcast
    Added missing columns

--- database/Tables/EnergyLogs.sql
+++ database/Tables/EnergyLogs.sql
@@ -5,5 +5,7 @@ CREATE TABLE EnergyLogs
     EnergyLogId INT PRIMARY KEY AUTO_INCREMENT,
     `Timestamp` DATETIME NOT NULL,
     TotalConsumption DECIMAL(12,2),
-    Delta DECIMAL(12,2)
+    Delta DECIMAL(12,2),
+    PowerDraw DECIMAL(12,2),
+    MeterBroadcast VARCHAR(250)
 );
