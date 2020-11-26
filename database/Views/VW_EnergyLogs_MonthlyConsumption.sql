9bd1106b20561b4c966617bea87d34e7f89fbc58
Angel Guevara, Jr <aguevara.716@gmail.com>
2020-11-26 15:12:57 -0500

    Create More Views
    Updated the MonthlyConsumption view to include the consumption cost

--- database/Views/VW_EnergyLogs_MonthlyConsumption.sql
+++ database/Views/VW_EnergyLogs_MonthlyConsumption.sql
@@ -2,13 +2,28 @@ DROP VIEW IF EXISTS VW_EnergyLogs_MonthlyConsumption;
 
 CREATE VIEW IF NOT EXISTS VW_EnergyLogs_MonthlyConsumption
 AS
-SELECT
-   DATE_FORMAT(`Timestamp`, '%Y-%m') AS `Month`,
-   SUM(`Delta`) AS `kWh`,
-   COUNT(*) AS `DataPoints`
-FROM 
-    `EnergyLogs` 
-GROUP BY 
-    'Month'
-ORDER BY
-    `Timestamp`;
+(
+    WITH MonthlyConsumption AS
+    (
+        SELECT
+            DATE_FORMAT(`Timestamp`, '%Y-%m-01') AS `Month`,
+            SUM(Delta) AS kWh,
+            COUNT(0) AS DataPoints
+        FROM
+            EnergyLogs
+        GROUP BY
+            `Month`
+    )
+    SELECT
+        `Month`,
+        DataPoints,
+        kWh,
+        CASE WHEN kWh <= 250
+            THEN kWh * 0.11125
+            ELSE ((250 * 0.11125) + ((kWh - 250) * 0.21788))
+        END AS 'Cost'
+    FROM
+        MonthlyConsumption
+    ORDER BY
+        `Month`
+)
